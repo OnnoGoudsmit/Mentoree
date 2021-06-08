3beec40b7c3ed20686ad5506fd52eadc8bc4d4a7
jose prieto <josel0priet0@gmail.com>
2021-06-08 11:29:50 +0200

    mentor seed started

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+require "open-uri"
+
+puts "Destroying mentor"
+Mentor.destroy_all
+
+puts 'Creating 4 fake Mentors...'
+puts 'Finished creating Mentors'
+
+puts "Creating a new Mentor"
+
+
