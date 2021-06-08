efd8f02e7a032be467c725f5a6ac2a9f9e53ae7a
Onno Goudsmit <onno.goudsmit@gmail.com>
2021-06-08 14:25:11 +0200

    added path to draft 2

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'draft', to: 'pages#draft'
+  get 'draft2', to: 'pages#draft2'
+
 
   resources :meetings, only: [:index]
 end
