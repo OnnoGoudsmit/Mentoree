0a17a4001d81d5110a4e520ced62f0a31f6bb882
Onno Goudsmit <onno.goudsmit@gmail.com>
2021-06-09 12:14:41 +0200

    added validation that mentoree can't be same as mentor

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -2,4 +2,14 @@ class Meeting < ApplicationRecord
   belongs_to :mentoree, class_name: :User, foreign_key: :mentoree_id
   belongs_to :mentor, class_name: :User, foreign_key: :mentor_id
   belongs_to :availability
+
+  validate :not_equal
+
+  private
+
+  def not_equal
+    if mentoree_id == mentor_id
+      errors.add(:mentoree_id, "Mentoree can't be the same person as the mentor")
+    end
+  end
 end
