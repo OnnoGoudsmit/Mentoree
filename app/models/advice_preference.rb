10922f42f6be377c9e34ef6cbfd52b663c17cc6e
Daniel Mendez <danielsaso@gmail.com>
2021-06-11 14:26:51 +0200

    advice prefrences form update

--- app/models/advice_preference.rb
+++ app/models/advice_preference.rb
@@ -3,4 +3,5 @@ class AdvicePreference < ApplicationRecord
   belongs_to :user
 
   SUBJECTS = ['Salary', 'Teamwork', 'Management', 'Work-life Balance', 'Freelancing', 'Soft Skills', 'Careerpath', 'Motivation', 'Jobhunting']
+
 end
