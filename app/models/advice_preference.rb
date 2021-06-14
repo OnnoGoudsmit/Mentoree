1e1b7db9504b54d2b4ebbb0ffada3fc6ccfe7b1a
Onno Goudsmit <onno.goudsmit@gmail.com>
2021-06-14 11:18:47 +0200

    Gave styling to radio buttons + some additional styling form

--- app/models/advice_preference.rb
+++ app/models/advice_preference.rb
@@ -2,6 +2,16 @@ class AdvicePreference < ApplicationRecord
   belongs_to :industry
   belongs_to :user
 
-  SUBJECTS = ['Salary', 'Teamwork', 'Management', 'Work-life Balance', 'Freelancing', 'Soft Skills', 'Careerpath', 'Motivation', 'Jobhunting']
+  SUBJECTS = {
+    Salary: ["Question 1",'Question 2'],
+    Teamwork: ["Question 3",'Question 4'],
+    Management: ["Question 5",'Question 6'],
+    "Work-life Balance": ["Question 7",'Question 8'],
+    Freelancing: ["Question 9",'Question 10'],
+    "Soft Skills": ["Question 11",'Question 12'],
+    Careerpath: ["Question 13",'Question 14'],
+    Motivation: ["Question 15",'Question 16'],
+    Jobhunting: ["Question 17",'Question 18']
+        }
 
 end
