ea33b243acb02419e424bc47d75f100f0c71b9bb
jose prieto <josel0priet0@gmail.com>
2021-06-16 12:56:58 +0200

    cahnges added to the review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -11,21 +11,22 @@ class ReviewsController < ApplicationController
     @review.meeting_id = @meeting.id
     if @review.save
     # we want to display the review in the mentor show page
-    # redirect_to after_sign_up_path(@meeting)
+      redirect_to after_sign_up_path(@meeting.mentoree_id)
     else
       render :new
     end
+    skip_authorization
   end
 
   def destroy
     @review = Review.find(params[:id])
     @review.destroy
-    # redirect_to after_sign_up_path(@review.meeting_id)
+    redirect_to after_sign_up_path(@review.meeting.mentoree_id)
   end
 
   private
 
   def review_params
-    params.require(:reviews).permit(:content, :rating)
+    params.require(:review).permit(:content, :rating)
   end
 end
